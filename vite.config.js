d665f739dfed56638f90e82aca6213b3d48e0ce6
Digital Asset <Digitalasste265@gmail.com>
2025-05-11 20:18:44 +0100

    Corrigido base para domínio personalizado

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/digital-asset-novo/', // substitui pelo nome do repositório, se for diferente
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist'
