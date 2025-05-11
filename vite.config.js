ae4c63a8fb16ba8902b74dd4f3c4f87e547b3e6d
Digital Asset <Digitalasste265@gmail.com>
2025-05-11 19:49:36 +0100

    Corrigido acesso direto às rotas com 404.html e base no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/digital-asset-novo/', // substitui pelo nome do repositório, se for diferente
   plugins: [react()],
   build: {
     outDir: 'dist'
